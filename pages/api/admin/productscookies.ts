2d8924b99d39967a0374fb77bb5e223239ae0653
Jonathan <jgonzalezf09@educantabria.es>
2022-08-24 20:10:18 +0200

    Terminado Lista Recomendados

--- pages/api/admin/productscookies.ts
+++ pages/api/admin/productscookies.ts
@@ -10,7 +10,6 @@ type Data =
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
     switch (req.method) {
         case 'DELETE':
-            console.log("LLegue ProductsCookiesGet");
             return getProductsCookies(req, res);
         default:
             res.status(400).json({ message: 'Bad Request' })
@@ -19,49 +18,58 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
 
 const getProductsCookies = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
-    console.log("LLegue ProductsCookiesGet 2");
-
+    // console.log("LLegue ProductsCookiesGet 2");
     
     const { userId = '', cookies = [] } = req.body;
-    console.log("Cookies: ");
-    console.log({cookies});
-    console.log("UserId: ");
-    console.log({userId});
+    // console.log("Cookies: ");
+    // console.log({cookies});
+    // console.log("UserId: ");
+    // console.log({userId});
 
     const orders = await Order.find({user: userId}).select('orderItems');
-    // const {orderItems} = orders.orderItems!;
-    // const orderJson = JSON.parse(JSON.stringify(orders));
-    // orders.length
-
-    const orderItems : IOrderItem[] = [];
-
-    for(var i = 0; i < orders.length ; i++){
-        console.log("Numero: "+ i + " -> " + orders[i].orderItems);
-        const iOrderItem : any = orders[i].orderItems;
-        orderItems.push(iOrderItem);
-    }
-
-    console.log(orderItems);
 
     const titles : any[] = [];
 
-    for(var i = 0; i < orderItems.length ; i++){
-        console.log("Title: "+ i + " -> " + orderItems[i].title);
-        const titleItem= orderItems[i].title;
-        titles.push(titleItem);
+    for(var i = 0; i < orders.length ; i++){
+        // console.log("Numero: "+ i + " -> " + orders[i].orderItems);
+        const orderItem : string =  ` ${orders[i].orderItems.toString()} `;
+        const title = orderItem.split(',');
+        const [first, second] = title[1].split(":");
+        titles.push(second);
     }
-
-
-    console.log(titles);
     
-    
+    const titlesOrders : string[] = [];
 
+    titles.map(t => {
+        const genero = t.toLowerCase();
+        if(genero.includes("men")){
+            titlesOrders.push('men');
+        }
+        if(genero.includes("woman")){
+            titlesOrders.push('woman');
+        }
+        if(genero.includes("kid")){
+            titlesOrders.push('kid');
+        }
+        if(genero.includes("unisex")){
+            titlesOrders.push('unisex');
+        }
+    });
+
+    // console.log(titlesOrders);
+
+    const resultTitleOrders = titlesOrders.filter((item,index)=>{
+        return titlesOrders.indexOf(item) === index;
+      });
 
     let condition = {};
 
-    // if(gender !== 'all' && SHOP_CONSTANTS.validGenders.includes(`${gender}`)){
-    //     condition = {gender};
-    // }
+    if (resultTitleOrders){
+        const randomCookie = Math.floor(Math.random()  * resultTitleOrders.length);
+        // console.log(randomCookie)
+        const gender = resultTitleOrders[randomCookie]
+        condition = {gender};
+    }
 
     await db.connect();
     const products = await Product.find(condition)
@@ -76,5 +84,7 @@ const getProductsCookies = async (req: NextApiRequest, res: NextApiResponse<Data
         return product;
     });
 
-    return res.status(200).json(updateProducts);
+    // console.log(updateProducts)
+
+    res.status(200).json(JSON.parse(JSON.stringify(updateProducts)));
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -16,47 +16,48 @@ interface Props {
 
 const HomePage: NextPage<Props> = ({ session }) => {
 
-  const {products, isError, isLoading} =  useProducts('/products');
-  const [productsCookies, setProductsCookies] = useState<IProduct[]>([]);
+  const { products, isError, isLoading } = useProducts('/products');
+  const [productsCookies, setProductsCookies] = useState<any[]>();
 
-  useEffect(() => {
-    
-    const cookies = Cookies.get('cart');
-    const userId = session.user._id || null;
-    console.log(cookies);
-    console.log(userId);
-    tesloApi.delete('admin/productscookies', { data: { cookies: Cookies.get('token'), userId: session.user._id || null} });
+  useMemo(() => {
+
+    tesloApi.delete('admin/productscookies', { data: { cookies: Cookies.get('token'), userId: session.user._id || null } }).then(res => {
+      if (res.data) {
+        setProductsCookies(res.data);
+      }
+    });
 
   }, [Cookies.get('token'), session.user._id])
-  
+
   if (isError) return <div>failed to load</div>
   if (!products) return <FullScreenLoading />
+  if (!productsCookies) return <FullScreenLoading />
 
   return (
     <ShopLayout title={'TesloShop - Home'} pageDescription={'Encuentra los mejores de Teslo aquí'}>
-      
+
       <Typography variant='h1' component='h1'>Tienda</Typography>
-      
+
 
       {/* TODO  _iD Y IProduct*/}
-      <Typography variant='h2' sx={{mb:3,  mt:3, fontWeight:'bolder' }}>Productos Recomendados para ti</Typography>
-      <ProductListCookies products={products} />
+      <Typography variant='h2' sx={{ mb: 3, mt: 3, fontWeight: 'bolder' }}>Productos Recomendados para ti</Typography>
+      <ProductListCookies products={productsCookies as IProduct[]} />
 
-      <Typography variant='h2' sx={{mb:3, mt:5, fontWeight:'bolder' }}>Todos los productos</Typography>
+      <Typography variant='h2' sx={{ mb: 3, mt: 5, fontWeight: 'bolder' }}>Todos los productos</Typography>
       <ProductList products={products} />
 
     </ShopLayout>
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async ({req}) => {
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
 
   const session: any = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
   return {
-      props: {
-          session
-      }
+    props: {
+      session
+    }
   }
 }
 
